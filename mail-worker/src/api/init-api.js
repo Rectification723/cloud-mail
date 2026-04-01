9d530738d4117812dcbd9a068df3f1a0ef4d5014
liuxiaoyuan <839107357@qq.com>
2026-04-01 17:22:42 +0800

    Add init env debug logging

--- mail-worker/src/api/init-api.js
+++ mail-worker/src/api/init-api.js
@@ -2,5 +2,12 @@ import app from '../hono/hono';
 import { dbInit } from '../init/init';
 
 app.get('/init/:secret', (c) => {
+	console.log('init debug', {
+		path: c.req.path,
+		hasJwtSecret: !!c.env.jwt_secret,
+		jwtSecretLength: c.env.jwt_secret ? String(c.env.jwt_secret).length : 0,
+		requestSecretLength: c.req.param('secret') ? String(c.req.param('secret')).length : 0,
+		envKeys: Object.keys(c.env || {}).sort()
+	});
 	return dbInit.init(c);
 })
